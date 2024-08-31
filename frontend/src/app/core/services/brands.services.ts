2dc2082cc7b7438787dd2ecd0cc202e3b48d9ab8
valentin_marchese <marchese52002@gmail.com>
2024-08-31 16:35:12 -0300

    createComponentBrand

--- frontend/src/app/core/services/brands.services.ts
+++ frontend/src/app/core/services/brands.services.ts
@@ -13,4 +13,13 @@ export class BrandsService {
   addBrand(brand: Brand): Observable<Brand> {
     return this.http.post<Brand>(this.apiUrl, brand);
   }
+  deleteBrand(brand: Brand): Observable<Brand> {
+    return this.http.delete<Brand>(`${this.apiUrl}/${brand.id}`);
+  }
+  editBrand(brand: Brand): Observable<Brand> {
+    return this.http.put<Brand>(`${this.apiUrl}/${brand.id}`, brand);
+  }
+  getAllBrand(): Observable<Brand[]> {
+    return this.http.get<Brand[]>(this.apiUrl);
+  }
 }
--- frontend/src/app/product/brand/brand.component.ts
+++ frontend/src/app/product/brand/brand.component.ts
@@ -1,38 +1,104 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BrandsService } from '../../core/services/brands.services.js';
+import { Brand } from '../../core/models/brands.interfaces.js';
 
 @Component({
   selector: 'app-brand',
   standalone: true,
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './brand.component.html',
   styleUrl: './brand.component.css'
 })
 export class BrandComponent implements OnInit {
 brandForm: FormGroup=new FormGroup({}); 
-
+brands: Brand[] = [];
+selectedBrand: Brand | null = null;
 constructor(
 private fb: FormBuilder, 
 private brandService: BrandsService
 )
- {}
+ {this.brandForm = this.fb.group({
+  nombreMarca: ['', Validators.required]
+  });}
 
 ngOnInit(): void {
-this.brandForm = this.fb.group({
-nombreMarca: ['', Validators.required]
-});
+this.loadBrand()
 }
 
-onSubmit() {
+
+openModal(modalId: string, brand: Brand): void{
+  this.selectedBrand = brand;
+  if (brand) {
+    this.brandForm.patchValue({
+      nombreMarca: brand.nombreMarca,
+    });
+  }
+  const modalDiv = document.getElementById(modalId);
+  if(modalDiv != null){
+    modalDiv.style.display='block';
+  }
+}
+
+closeModal(modalId: string){
+  const modalDiv = document.getElementById(modalId);
+  if(modalDiv != null){
+    modalDiv.style.display='none';
+  }
+  const backdrop = document.querySelector('.modal-backdrop');
+  if (backdrop != null) {
+    backdrop.parentNode?.removeChild(backdrop);
+  }
+  this.selectedBrand= null;
+}
+
+addBrand() {
   if (this.brandForm.valid) {
     const brandData = this.brandForm.value;
   
-    this.brandService.addBrand(brandData).subscribe((brand) => {
+    this.brandService.addBrand(brandData).subscribe(() => {
       alert('Marca agregada' );
+      this.brandForm.reset();
+      this.closeModal('addBrand');
+      this.ngOnInit();   
+    
+
+    });
+  }
+
+}
+
+loadBrand(): void {
+  this.brandService.getAllBrand().subscribe((brands : Brand[]) => {
+    this.brands = brands;
+  });
+}
+
+editBrand(): void {
+  if (this.selectedBrand) {
+    const updatedBrand: Brand = {
+      ...this.selectedBrand,
+      ...this.brandForm.value
+    };
+    this.brandService.editBrand(updatedBrand).subscribe(() => {
+      alert('Marca actualizada');
+      this.closeModal('editBrand');
+      this.loadBrand();
       this.brandForm.reset();
     });
   }
 }
+
+deleteBrand(brand: Brand | null, modalId: string) {
+  if(brand){
+      this.brandService.deleteBrand(brand).subscribe(() => {
+        alert('Marca eliminada');
+        this.ngOnInit();
+        this.closeModal(modalId);
+        this.brandForm.reset();
+      });
+  }
 }
+}
+

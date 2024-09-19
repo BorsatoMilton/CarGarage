f5dafad9d87b97e9575eb114e5b11c429198e29e
BorsatoMilton <borsatomilton@gmail.com>
2024-09-19 18:07:17 -0300

    Remove dist from version control

--- backend/dist/correo/correo.routes.js
+++
@@ -1,5 +0,0 @@
-import { Router } from 'express';
-import { envioCorreo } from './correo.controller.js';
-export const correoRouter = Router();
-correoRouter.post('/', envioCorreo);
-//# sourceMappingURL=correo.routes.js.map
--- backend/dist/shared/db/tokenGenerator.js
+++
@@ -1,5 +0,0 @@
-import crypto from 'crypto';
-export function generateToken() {
-    return crypto.randomBytes(32).toString('hex');
-}
-//# sourceMappingURL=tokenGenerator.js.map
